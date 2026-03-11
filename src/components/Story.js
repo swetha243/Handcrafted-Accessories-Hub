990a8918a16fce7305d5758832d7866935c95be9
Lalitha Swetha <140937019+swetha243@users.noreply.github.com>
2026-03-12 00:54:44 +0530

    Add files via upload

---
+++ src/components/Story.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Story() {
+  return (
+    <section className="story">
+
+      <h2>Our Story</h2>
+
+      <p>
+      Handcrafted Accessories Hub is created to showcase beautiful handmade
+      accessories made with creativity and passion. Each product is carefully
+      crafted to bring uniqueness and style to everyday fashion.
+      </p>
+
+    </section>
+  );
+}
+
+export default Story;
